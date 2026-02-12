598a62bd40e6aa4b84e0a15e4c887c7a5a9946e7
Joel <joel@gluth.io>
2026-02-12 12:14:28 +0000

    Add basic tile map implementation
    - create-tile-map: creates map with width, height, and floor tiles
    - get-tile/set-tile: coordinate-based tile access
    - in-bounds?: boundary checking
    - Flat vector storage with coordinate-to-index conversion

--- src/yarf/core.clj
+++ src/yarf/core.clj
@@ -1,6 +1,43 @@
 (ns yarf.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def default-tile {:type :floor})
+
+(defn create-tile-map
+  "Creates a tile map with the given width and height, filled with default tiles."
+  [width height]
+  {:width width
+   :height height
+   :tiles (vec (repeat (* width height) default-tile))})
+
+(defn map-width
+  "Returns the width of the tile map."
+  [tile-map]
+  (:width tile-map))
+
+(defn map-height
+  "Returns the height of the tile map."
+  [tile-map]
+  (:height tile-map))
+
+(defn in-bounds?
+  "Returns true if the coordinates are within the map bounds."
+  [tile-map x y]
+  (and (>= x 0)
+       (>= y 0)
+       (< x (:width tile-map))
+       (< y (:height tile-map))))
+
+(defn- coords->index
+  "Converts x,y coordinates to a flat array index."
+  [tile-map x y]
+  (+ x (* y (:width tile-map))))
+
+(defn get-tile
+  "Returns the tile at the given coordinates."
+  [tile-map x y]
+  (get (:tiles tile-map) (coords->index tile-map x y)))
+
+(defn set-tile
+  "Returns a new tile map with the tile at coordinates set to the given value."
+  [tile-map x y tile]
+  (assoc-in tile-map [:tiles (coords->index tile-map x y)] tile))
--- test/yarf/core_test.clj
+++ test/yarf/core_test.clj
@@ -2,6 +2,34 @@
   (:require [clojure.test :refer :all]
             [yarf.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest create-tile-map-test
+  (testing "creates a tile map with specified dimensions"
+    (let [m (create-tile-map 10 8)]
+      (is (= 10 (map-width m)))
+      (is (= 8 (map-height m))))))
+
+(deftest get-tile-test
+  (testing "returns default tile for empty map"
+    (let [m (create-tile-map 5 5)]
+      (is (= :floor (:type (get-tile m 0 0))))
+      (is (= :floor (:type (get-tile m 4 4)))))))
+
+(deftest set-tile-test
+  (testing "sets a tile at coordinates"
+    (let [m (create-tile-map 5 5)
+          m2 (set-tile m 2 3 {:type :wall})]
+      (is (= :wall (:type (get-tile m2 2 3))))
+      (is (= :floor (:type (get-tile m2 0 0)))))))
+
+(deftest bounds-check-test
+  (testing "in-bounds? returns true for valid coordinates"
+    (let [m (create-tile-map 10 10)]
+      (is (in-bounds? m 0 0))
+      (is (in-bounds? m 9 9))
+      (is (in-bounds? m 5 5))))
+  (testing "in-bounds? returns false for invalid coordinates"
+    (let [m (create-tile-map 10 10)]
+      (is (not (in-bounds? m -1 0)))
+      (is (not (in-bounds? m 0 -1)))
+      (is (not (in-bounds? m 10 0)))
+      (is (not (in-bounds? m 0 10))))))
